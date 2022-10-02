cdca1928e4cb49e3f549fe1bd8d8dbf20ffa9ee1
Camilo Urrego <ceurrego@bancofalabella.com.co>
2022-10-02 15:55:00 -0500

    copia ejemplo mern movies app

---
+++ client/src/app/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import { NavBar } from '../components'
+import { MoviesInsert, MoviesUpdate } from '../pages'
+import MoviesList from '../pages/MoviesList'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/movies/list"  exact element={<MoviesList/>} />
+                <Route path="/movies/create" exact element={<MoviesInsert/>} />
+                <Route path="/movies/update/:id" exact element={<MoviesUpdate/>} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App
